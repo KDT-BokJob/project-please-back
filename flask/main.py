57e10ea436e0637a71dbda28ba78e35160544a69
Hyeon-hak Kim <07ily@naver.com>
2023-10-22 12:56:02 +0900

    ✨ Feat: add templates/hello.html \& update main.py
    Dynamic Routing via `hello/\<name\>`

--- flask/main.py
+++ flask/main.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from dotenv import dotenv_values
+from flask import render_template
 
 app = Flask(__name__)
-config = dotenv_values("env/.flaskenv")
+config = dotenv_values(".flaskenv")
 
 
 @app.route("/")
 def hello_world():
-    return ('<a href="http://localhost:{}">Next</a><br>'.format(config["NEXT_RUN_PORT"]) +
-            '<a href="http://localhost:{}">Django</a><br>'.format(config["FLASK_RUN_PORT"]) +
-            '<a href="http://localhost:{}">Spring</a>'.format(config["SPRING_RUN_PORT"]))
+    return '<h1>{}</h1>'.format(__name__)
 
 
-app.run()
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template(
+        'hello.html',
+        name=name,
+        FLASK_RUN_PORT=config["FLASK_RUN_PORT"],
+        SPRING_RUN_PORT=config["SPRING_RUN_PORT"])
+
+
+if __name__ == '__main__':
+    app.run()
