43fc91cfae429bcc081f420d81e9fd4e814ed816
YataNox <rladnwls0523@naver.com>
2023-10-29 17:45:54 +0900

    Feat: 공고 api CRUD 작성 중 list 조회 등의 api는 아직 미작성 상황 추가 예정

--- spring/src/main/java/com/kdt/please/domain/recruit/controller/RecruitController.java
+++ spring/src/main/java/com/kdt/please/domain/recruit/controller/RecruitController.java
@@ -1,18 +1,39 @@
 package com.kdt.please.domain.recruit.controller;
 
 import com.kdt.please.domain.recruit.service.request.RecruitCreateRequest;
+import com.kdt.please.domain.recruit.service.request.RecruitUpdateRequest;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/recruit")
+@RestController
+@RequestMapping("/recruit")
 public class RecruitController {
-    /*@GetMapping("/")
-    public ResponseEntity<String> getRecruit(){
 
-    }*/
+    @ApiOperation("공고 등록")
+    @PostMapping("/{companyId}")
+    public ResponseEntity<Void> createRecruit(@ApiParam(value = "기업ID") @PathVariable(value = "conpanyId") Long conpanyId,
+                                              @RequestBody RecruitCreateRequest recruitCreateRequest){
+        return ResponseEntity.ok().build();
+    }
 
-    @PostMapping("/create")
-    public ResponseEntity<Void> createRecruit(@RequestBody RecruitCreateRequest recruitCreateRequest){
+    @ApiOperation("공고 수정")
+    @PutMapping("/{postId}")
+    public ResponseEntity<String> updateRecruit(@ApiParam(value = "공고ID") @PathVariable(value = "postId") Long postId,
+                                                @RequestBody RecruitUpdateRequest recruitUpdateRequest){
+        return ResponseEntity.ok().build();
+    }
+
+    @ApiOperation("공고 조회")
+    @GetMapping("/{postId}")
+    public ResponseEntity<String> getRecruit(@ApiParam(value = "공고ID") @PathVariable(value = "postId") Long postId){
+        return ResponseEntity.ok().build();
+    }
+
+    @ApiOperation("공고 삭제")
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<String> deleteRecruit(@ApiParam(value = "공고ID") @PathVariable(value = "postId") Long postId){
         return ResponseEntity.ok().build();
     }
 }
--- spring/src/main/java/com/kdt/please/domain/recruit/service/request/RecruitUpdateRequest.java
+++ spring/src/main/java/com/kdt/please/domain/recruit/service/request/RecruitUpdateRequest.java
@@ -2,17 +2,19 @@ package com.kdt.please.domain.recruit.service.request;
 
 import java.time.LocalDateTime;
 
-public record RecruitCreateRequest(
+public record RecruitUpdateRequest(
+        // 공고 ID
+        Long postId,
         // 구인자 ID
-        Long recruiterId,
+        //Long recruiterId,
         // 기업 ID
-        Long companyId,
+        //Long companyId,
         // 제목
         String title,
         // 내용
         String content,
         // 작성일
-        LocalDateTime createAt,
+        //LocalDateTime createAt,
         // 마감일
         LocalDateTime endTime,
         // 급여
