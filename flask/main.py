06345b88b78bc927e0540829745f6b87f8adce06
Hyeon-hak Kim <07ily@naver.com>
2023-11-03 10:42:45 +0900

    Feat: add HEXACO test
    - /questions [GET]
    Get 100 questions for the test
    - /questions/jtc [POST]
    Return HEXACO from test result
    
    Signed-off-by: Hyeon-hak Kim <07ily@naver.com>

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+import pandas as pd
 from dotenv import dotenv_values
+from flask import Flask, request, jsonify
 from flask import render_template
+
+from src.rec import asd, calc_result
 
 app = Flask(__name__)
 config = dotenv_values(".flaskenv")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html', error='{}'.format(404)), 404
 
 
 @app.route("/")
@@ -21,5 +29,50 @@ def hello(name=None):
         SPRING_RUN_PORT=config["SPRING_RUN_PORT"])
 
 
+@app.route('/fetch/example')
+def api():
+    return render_template('base.html')
+
+
+@app.route('/echo/<param>')
+def get_echo_call(param):
+    return jsonify({"param": param})
+
+
+@app.route('/echo', methods=['POST'])
+def post_echo_call():
+    param = request.get_json()
+    return jsonify(param)
+
+
+@app.route('/join', methods=['GET'])
+def get_join():
+    query = request.args.get('query')
+    data = {'status_code': 400, 'method': request.method, 'query': query}
+    return jsonify(data)
+
+
+@app.route('/questions', methods=['GET'])
+def questions_get(questions100_list=None):
+    json = {"data": questions100_list}
+    return jsonify(json)
+
+
+@app.route('/questions', methods=['POST'])
+def questions_post():
+    param = request.get_json()
+    return jsonify(param)
+
+
+@app.route('/questions/jtc', methods=['POST'])
+def json_to_csv():
+    data = request.get_json()
+    df = pd.json_normalize(data["Results"])
+    df.transpose().to_csv("./data/temp.csv")
+    res = calc_result("./data/temp.csv")
+    return jsonify({"data": res})
+
+
 if __name__ == '__main__':
+    db = asd()
     app.run()
