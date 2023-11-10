c9e8311fac288d85ef229537445968ceb5871e92
Hyeon-hak Kim <07ily@naver.com>
2023-11-10 23:17:51 +0900

    ✨ Feat: min-max Normalization

--- flask/src/recommendation/rec.py
+++ flask/src/recommendation/rec.py
@@ -61,14 +61,25 @@ def EDA(df):
         ls = df[key].unique()
         if len(ls) < 20:
             print(f"{key}:{len(ls)} {ls}")
-    # print(df.info)
+    print(df.info())
     print(df[:3].T)
 
 
 def preprocessing(df):
-    nums = ['age', 'period', 'korean_proficiency', 'day_remains', 'total_day_worked', 'total_indicators',
-            'is_experienced', 'is_disabled']
-    cats = ['address', 'gender', 'nationality', 'visa']
+    str_type = ['address', 'gender', 'nationality', 'visa']
+    float_type = ['age']
+    int_type = ['period', 'korean_proficiency', 'day_remains', 'total_day_worked', 'total_indicators']
+    bool_type = ['is_experienced', 'is_disabled']
+
+    for key in str_type:
+        df[key] = df[key].astype(str)
+    for key in float_type:
+        df[key] = df[key].astype(str).astype(float)
+    for key in int_type:
+        df[key] = df[key].astype(str).astype(int)
+
+    nums = float_type + int_type + bool_type
+    cats = str_type
 
     # one-hot-encoding
     for key in cats:
@@ -76,14 +87,21 @@ def preprocessing(df):
         df = pd.concat([df, dum], axis='columns')
         df.pop(key)
 
+    # filter
+    df = df[(19 <= df['age'])]
+    df = df[(df['age'] < 60)]
+
     # normalize
     for key in nums:
         mn, mx = df[key].min(), df[key].max()
-    df[key].apply(lambda x: (x - mn) / (mx - mn))
+        if mn < mx and key not in bool_type:
+            df[key] = df[key].apply(lambda x: (x - mn) / (mx - mn))
+
     return df
 
 
 user_df = get_user_df()
 # EDA(user_df)
 user_df = preprocessing(user_df)
+print(user_df.info())
 print(user_df[:3].T)
