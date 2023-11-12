15a6bfad27e013f5c9f1097ba8837baf98287f59
YataNox <rladnwls0523@naver.com>
2023-11-12 14:27:09 +0900

    ✨ Feat : apply api 작성중
    userId에 따른 지원 목록 조회(페이징)

--- spring/src/main/java/com/kdt/please/domain/apply/repository/ApplyRepository.java
+++ spring/src/main/java/com/kdt/please/domain/apply/repository/ApplyRepository.java
@@ -1,7 +1,10 @@
 package com.kdt.please.domain.apply.repository;
 
-import com.kdt.please.domain.recruit.Recruit;
+import com.kdt.please.domain.apply.Apply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApplyRepository extends JpaRepository<Recruit, Long> {
+public interface ApplyRepository extends JpaRepository<Apply, Long> {
+    Page<Apply> findAllByUser_UserId(Long userId, Pageable pageable);
 }
--- spring/src/main/java/com/kdt/please/global/Status.java
+++ spring/src/main/java/com/kdt/please/global/Status.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum Status {
 
     APPLIED("APPLIED"), // 지원
-    RESUME_REJECTED(" RESUME_REJECTED"), // 이력서 탈락
+    RESUME_DEFAULT_REJECTED("RESUME_DEFAULT_REJECTED"), // 이력서 탈락
     INTERVIEW_SELECTED("INTERVIEW_SELECTED"), // 면접자 선정
     FINAL_PASSED("FINAL_PASSED"), // 최종 합격
     FINAL_REJECTED("FINAL_REJECTED"); // 최종 탈락
