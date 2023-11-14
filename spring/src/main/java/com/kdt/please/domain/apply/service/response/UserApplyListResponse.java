eb9f2696edc4ccd7ed9ed0801558d288638d4552
YataNox <rladnwls0523@naver.com>
2023-11-14 20:59:34 +0900

    ✨ Feat : 유저 지원 내역 Response 작성

---
+++ spring/src/main/java/com/kdt/please/domain/apply/service/response/UserApplyListResponse.java
@@ -0,0 +1,50 @@
+package com.kdt.please.domain.apply.service.response;
+
+import com.kdt.please.domain.apply.Apply;
+import com.kdt.please.domain.resume.Resume;
+import lombok.Builder;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
+@Builder
+public record UserApplyListResponse(
+
+        @NotNull @Positive
+        Long resumeId,
+
+        @NotNull
+        String firstName,
+
+        @NotNull
+        String lastName,
+
+        @NotNull
+        String gender,
+
+        @NotNull
+        String age,
+
+        @NotNull
+        Boolean isExperienced,
+
+        @NotNull
+        String address,
+
+        @NotNull
+        String nationality
+) {
+    public static UserApplyListResponse from(Resume resume){
+        return UserApplyListResponse.builder()
+                .resumeId(resume.getResumeId())
+                .firstName(resume.getFirstName())
+                .lastName(resume.getLastName())
+                .gender(resume.getGender())
+                .age(String.valueOf(LocalDate.now().getYear() - resume.getBirthdate().getYear()))
+                .isExperienced(resume.isExperienced())
+                .address(resume.getAddress())
+                .nationality(resume.getNationality())
+                .build();
+    }
+}
