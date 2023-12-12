cb98d5e99de56dba967e9dc3510f887351a0fed2
Joohee <ljh0667@gmail.com>
2023-12-12 20:38:54 +0900

    🐛 Fix : 유저 로그 출력

--- spring/src/main/java/com/kdt/please/config/auth/CustomOAuth2UserService.java
+++ spring/src/main/java/com/kdt/please/config/auth/CustomOAuth2UserService.java
@@ -5,6 +5,7 @@ import com.kdt.please.config.auth.dto.SessionUser;
 import com.kdt.please.domain.user.User;
 import com.kdt.please.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpSession;
 import java.util.Collections;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
@@ -34,6 +36,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
+        log.info("user = {}", user.getEmail());
 
         httpSession.setAttribute("user", new SessionUser(user));
 
